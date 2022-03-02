101542387da4c6e61ac03e608ad5cb56b8bb6be7
Nikila Banuka Perera <78866528+Nikilaperera@users.noreply.github.com>
2022-03-02 21:54:20 +0530

    bug fixed

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -49,13 +49,7 @@
 import axios from 'axios'
 
 export default {
-  name: 'SingleMovie',
-
-  data() {
-    return {
-      movie: '',
-    }
-  },
+  name: 'singleMovie',
 
   async fetch() {
     await this.getSingleMovie()
@@ -69,10 +63,16 @@ export default {
       title: this.movie.title,
     }
   },
+
+  data() {
+    return {
+      movie: '',
+    }
+  },
   methods: {
     async getSingleMovie() {
       const data = axios.get(
-        `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`
+        `https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US`
       )
       const result = await data
       this.movie = result.data
